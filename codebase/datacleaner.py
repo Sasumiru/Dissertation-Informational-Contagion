0d3f58e90ff4bdfca02f9bb326903ec390b4a1bb
Sasumiru <samir.mobarak@outlook.com>
2026-09-25 15:07:56 +0100

    fixed filterexpose

--- codebase/datacleaner.py
+++ codebase/datacleaner.py
@@ -6,21 +6,27 @@ DATA = "data" #
 PLACEHOLDER = "XXXXXXXXXXXXXX" 
 
 def filterExpose():
-    column = ["LEI", "Period", "Item", "NACE", "Value"] #defining the columns that match the csv
+    column = ["LEI_Code", "Period", "Item", "NACE_codes", "Amount"] #defining the columns that match the csv
     strType = {
-        "LEI": str,
+        "LEI_Code": str,
         "Period": str,
         "Item": str,
-        "NACE": str,
+        "NACE_codes": str,
     }
     df = pd.read_csv(f"data/tr_cre.csv", usecols=column, dtype=strType) #reading the data
-    
+    df = df.rename(columns={"LEI_Code": "LEI", "NACE_codes": "NACE", "Amount": "Value"}) #shorter names
+
     df["Value"] = pd.to_numeric(df["Value"], errors="coerce").fillna(0) #if number isnt number = 0
 
-    startItem = df["Item"] == 2521301 #starting item in the csv file
-    startPeriod = df["Period"] == 202506 #starting period in the csv file
+    startItem = df["Item"] == "2521301" #starting item in the csv file
+    startPeriod = df["Period"] == "202506" #starting period in the csv file
 
     df = df[startItem & startPeriod] 
     df = df[df["NACE"] != 0] # if not a sector we remove it
     df = df[df["LEI"].ne(PLACEHOLDER)]
     return df
+
+if __name__ == "__main__":
+    df = filterExpose()
+    print(df.head())
+    print(len(df))
