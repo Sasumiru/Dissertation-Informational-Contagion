e4e2ce729f6a34392c8fd652da568d571780497b
Sasumiru <samir.mobarak@outlook.com>
2026-09-25 15:00:13 +0100

    Initial commit

---
+++ codebase/datacleaner.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity #iimports
+TEST1 = "test1" 
+DATA = "data" #
+PLACEHOLDER = "XXXXXXXXXXXXXX" 
+
+def filterExpose():
+    column = ["LEI", "Period", "Item", "NACE", "Value"] #defining the columns that match the csv
+    strType = {
+        "LEI": str,
+        "Period": str,
+        "Item": str,
+        "NACE": str,
+    }
+    df = pd.read_csv(f"data/tr_cre.csv", usecols=column, dtype=strType) #reading the data
+    
+    df["Value"] = pd.to_numeric(df["Value"], errors="coerce").fillna(0) #if number isnt number = 0
+
+    startItem = df["Item"] == 2521301 #starting item in the csv file
+    startPeriod = df["Period"] == 202506 #starting period in the csv file
+
+    df = df[startItem & startPeriod] 
+    df = df[df["NACE"] != 0] # if not a sector we remove it
+    df = df[df["LEI"].ne(PLACEHOLDER)]
+    return df
